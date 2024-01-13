bdbca89bc8853a21f843dffd0ad413a28a653a52
Gabriel Magioli <gabrielmagioli1@gmail.com>
2024-01-13 20:05:40 -0300

    Making some backend routes private and refactoring frontend

--- frontend/frontend-restaurant/src/App.jsx
+++ frontend/frontend-restaurant/src/App.jsx
@@ -3,7 +3,7 @@ import Login from './pages/login/Login.jsx';
 import LoginAdm from './pages/loginAdm/loginAdm.jsx';
 import Cadastro from './pages/cadastro/cadastro.jsx';
 import PgInicial from './pages/pgInicial/pgInicial.jsx';
-{/*import Perfil from '.pages/perfil/perfil.jsx';*/}
+import Perfil from '.pages/perfil/perfil.jsx';
 
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
           <Route path="/signup" element={<LoginAdm/>}/>
           <Route path="/cadastro" element={<Cadastro/>}/>
           <Route path="/pginicial" element={<PgInicial/>}/>
-          {/*<Route path="/perfil" element={<Perfil/>}/>*/}
+          <Route path="/perfil" element={<Perfil/>}/>
         </Routes>
       </BrowserRouter>
     </>
