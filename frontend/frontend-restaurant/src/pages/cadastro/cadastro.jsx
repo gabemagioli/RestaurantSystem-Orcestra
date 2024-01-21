4273236cebe9320c215f2aa40bf97233605a7e55
Gabriel Magioli <gabrielmagioli1@gmail.com>
2024-01-20 23:49:35 -0300

    Alteracoes no consumo da API cadastro

--- frontend/frontend-restaurant/src/pages/cadastro/cadastro.jsx
+++ frontend/frontend-restaurant/src/pages/cadastro/cadastro.jsx
@@ -3,36 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios'
 import './cadastro.css';
 
-const Signup = () =>{
+
+  
+function Cadastro() {
+
+  const navigate = useNavigate();
+  
   const[name, setName] = useState('');
   const[email, setEmail] = useState('');
   const[password, setPassword] = useState('');
   const[confirmpassword, setConfirmPassword] = useState('');
-
-
+  
+  
   const handleSignup = () =>{
     if (password != confirmpassword){
       console.error('As senhas não são iguais.');
       return;
     }
+      axios.post('http://localhost:8080/register/client', { name, email, password })
+      .then(response => {
+      // envio do cadastro pro servidor
+      console.log('Cadastro bem-sucedido:', response.data);
+      navigate("/");
+    })
+      .catch(error => {
+      // pegar o erro 
+      console.error('Erro no cadastro:', error);
+    });
   }
-  axios.post('http://localhost:5173/register/client', { name, email, password })
-        .then(response => {
-          // envio do cadastro pro servidor
-          console.log('Cadastro bem-sucedido:', response.data);
-        })
-        .catch(error => {
-          // pegar o erro 
-          console.error('Erro no cadastro:', error);
-        });
-      };
-
-function Cadastro() {
-  const navigate = useNavigate();
-    const handleNavigateLogin = () => {
-    navigate("/");
-  }
-
+      
   return (
     <body>
       <section className="engloba-login">
@@ -43,22 +42,36 @@ function Cadastro() {
         <div className="login">
           <div className="campo">
             <label htmlFor="nome" className="label">Nome: </label>
-            <input type="text" id="nome" name="nome" placeholder="Digite seu nome" />
+            <input 
+              type="text" 
+              id="nome" 
+              name="nome" 
+              placeholder="Digite seu nome" 
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="campo">
             <label htmlFor="email" className="label">Email: </label>
-            <input type="text" id="email" name="email" placeholder="Digite seu email" />
+            <input 
+              type="text" 
+              id="email" 
+              name="email" 
+              placeholder="Digite seu email" 
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="campo">
             <label htmlFor="senha" className="label">Senha: </label>
-            <input type="password" id="senha" name="senha" placeholder="Digite sua senha" />
+            <input 
+            type="password" 
+            id="senha" name="senha" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
           </div>
           <div className="campo">
             <label htmlFor="senha-nova" className="label">Confirmação de senha: </label>
-            <input type="password" id="senha-nova" name="senha" placeholder="Digite sua senha novamente" />
+            <input type="password" id="senha-nova" name="senha" placeholder="Digite sua senha novamente" onChange={(e) => setConfirmPassword(e.target.value)} />
           </div>
         </div>
-        <button className="cadastrar" onClick={handleNavigateLogin}>CONCLUIR</button> {/*navegacao para login ok*/}
+        <button className="cadastrar" onClick={handleSignup}>CONCLUIR</button> {/*navegacao para login ok*/}
       </section>
     </body>
   );
